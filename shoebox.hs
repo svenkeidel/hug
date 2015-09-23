805651e9aac586c3b8be0b23182799d2e1d6f9b7
Sven Keidel <svenkeidel@googlemail.com>
2015-09-23 22:36:55 +0200

    After second session

--- shoebox.hs
+++ shoebox.hs
@@ -1,39 +1,82 @@
 {-# LANGUAGE OverloadedStrings #-}
+module ShoeOne where
 
-module ShoeOne ()
-where
-
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Map as M
-import qualified Control.Applicative as CA
+import Data.Maybe
+import Control.Applicative
 
 -- types of databases
-type ShoeLexiconDB = M.Map T.Text T.Text    -- base words, lexicon
-type ShoeSuffixDB = M.Map T.Text T.Text   	-- suffixes
-type ShoeParsingDB = M.Map T.Text [T.Text]  -- parsing
+type ShoeParsingDB = M.Map Text [MorphemeBreak]  -- parsing
+type ShoeLexiconDB = M.Map LexEl [Meaning]       -- base words, lexicon
+type ShoeSuffixDB  = M.Map SuffixEl [Abbreviation] -- suffixes
 
+type SuffixEl = Text
+type Abbreviation = Text
+type Meaning = Text
+type LexEl  = Text  -- inside lexicon
+type FullEl = Text -- full element
+type Suffix = Text -- suffix
+
+data Gloss = Abbreviation Abbreviation
+           | Meaning Meaning
+  deriving (Show,Eq)
+
+newtype MorphemeBreak = MB [Morpheme]
+  deriving (Show,Eq)
+
+data Morpheme = MorphemeLex LexEl | MorphemeSuffix Suffix
+  deriving (Show,Eq)
+
+data InterlinearBlock = ILB MorphemeBreak [Gloss]
+  deriving (Show,Eq)
+
+type TextEl = Text
 
 shoeLexiconDB :: ShoeLexiconDB
-shoeLexiconDB = M.fromList [
-	("maison","house")
-	]
+shoeLexiconDB = M.fromList
+  [ ("maison",["house", "building"])
+  ]
 
 shoeSuffixDB :: ShoeSuffixDB
-shoeSuffixDB = M.fromList [
-	("s","PL")
-	]
+shoeSuffixDB = M.fromList
+  [ ("s",["PL"])
+  ]
 
 shoeParsingDB :: ShoeParsingDB
-shoeParsingDB = M.fromList [
-	("maisons",["maison", "s"])
-	]
+shoeParsingDB = M.fromList
+   [ ("maisons", [ MB [ MorphemeLex "maison"
+                      , MorphemeSuffix "s"
+                      ]
+                 ]
+     )
+   , ("cadeaux", [ MB [ MorphemeLex "cadeau"
+                      , MorphemeSuffix "s"
+                      ]
+                 ]
+     )
+   ]
 
 type ShoeDB = (ShoeLexiconDB, ShoeSuffixDB, ShoeParsingDB)
+
+shoeDB :: ShoeDB
 shoeDB = (shoeLexiconDB, shoeSuffixDB, shoeParsingDB)
 
-helper :: Maybe T.Text -> Maybe [T.Text]
-helper Nothing = Nothing
-helper (Just val) = Just [val]
+glossTX :: TextEl -> ShoeParsingDB -> [MorphemeBreak]
+glossTX textEl parsingDB =
+  fromMaybe [MB [MorphemeLex textEl]]
+    (M.lookup textEl parsingDB)
 
-gloss :: T.Text -> ShoeDB -> Maybe [T.Text]
-gloss w (ldb, sdb, pdb) = M.lookup w pdb CA.<|> helper (M.lookup w sdb) CA.<|> helper (M.lookup w ldb)
+glossMB :: MorphemeBreak -> ShoeLexiconDB -> ShoeSuffixDB -> [Gloss]
+glossMB (MB mbs) lexiconDB suffixDB =
+  concatMap go mbs
+  where
+    go (MorphemeLex l)    = Meaning <$>      fromMaybe [] (M.lookup l lexiconDB)
+    go (MorphemeSuffix s) = Abbreviation <$> fromMaybe [] (M.lookup s suffixDB)
+
+gloss :: TextEl -> ShoeDB -> [InterlinearBlock]
+gloss textEl (lexiconDB,suffixDB,parsingDB) = do
+  morphemeBreak <- glossTX textEl parsingDB
+  let glosses = glossMB morphemeBreak lexiconDB suffixDB
+  return $ ILB morphemeBreak glosses
