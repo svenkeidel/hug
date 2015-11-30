f6faf37c7155392214c3f893ff1c39f28c712902
Urs of the Backwoods <uotbw@hgamer3d.org>
2015-11-30 20:05:34 +0100

    parser works for DB files

--- shoebox/src/DatabaseParser.hs
+++ shoebox/src/DatabaseParser.hs
@@ -28,23 +28,38 @@ data DBRecord = DBRecord
             , meaning :: [Text]
             , lesson :: Maybe Text
             , comment :: Maybe Text
-            , date :: Maybe Day
+            , date :: Maybe Text
             }
+            deriving (Show)
 
+newRecord = DBRecord "" (-1) [] Nothing Nothing Nothing
 
-parseDatabase :: FilePath -> IO String
+updateRecord :: DBRecord -> DBElem -> DBRecord
+updateRecord r elem = case elem of
+  LE t -> r {lexicalEntry = t}
+  UUID t -> r {uuid = read (T.unpack t)}
+  ME t -> r {meaning = t}
+  LK t -> r {lesson = t}
+  CO c -> r {comment = c}
+  DT d -> r {date = d}
+  _ -> r
+
+parseDatabase :: FilePath -> IO [DBRecord]
 parseDatabase dbFile = do
    dbTxt <- T.readFile dbFile
    let result = case P.parse parseDB dbFile dbTxt of
-                        Left err -> show err
-                        Right val -> show val
-   return result
+                        Left _ -> []
+                        Right val -> val
+   let rfrom = \rec -> foldl updateRecord newRecord rec
+   let r = map rfrom result
+   return r
 
 -- base delimiters and sections, to cope with \n in between
 -- the rules are as follows:
 -- a "line" starts with a newline + "\"
 -- a "section" separator is a newline followed by a line starter
 
+spaces = P.many (P.oneOf " \t")     -- own spaces without newline
 parseSecSep = P.try (P.newline >> P.many1 P.newline)
 parseElemSep = P.try (P.newline >> (P.lookAhead (P.char '\\')))
 parseElemChar = do
@@ -69,43 +84,43 @@ parseDBElem = P.try parseLexicalElement
 
 parseLexicalElement :: Parsec Text () DBElem
 parseLexicalElement = do
-  P.string "\\le" >> P.spaces 
+  P.string "\\le" >> spaces 
   t <- P.many parseElemChar
   return (LE (T.pack t))
 
 parseUUID :: Parsec Text () DBElem
 parseUUID = do
-  P.string "\\_no" >> P.spaces
+  P.string "\\_no" >> spaces
   t <- P.many parseElemChar
   return (UUID (T.pack t))
 
 parseMeaning :: Parsec Text () DBElem
 parseMeaning = do
-  P.string "\\me" >> P.spaces
-  ts <- P.sepEndBy (P.many parseElemChar) (P.char ';')
+  P.string "\\me" >> spaces
+  ts <- P.sepEndBy (P.many parseElemChar) (P.char ';' >> spaces)
   return (ME (map T.pack ts))
 
 parseLesson :: Parsec Text () DBElem
 parseLesson = do
-  P.string "\\lk" >> P.spaces
+  P.string "\\lk" >> spaces
   t <- P.many parseElemChar
   return (LK (if t == [] then Nothing else Just (T.pack t)))
 
 parseComment :: Parsec Text () DBElem
 parseComment = do
-  P.string "\\co" >> P.spaces
+  P.string "\\co" >> spaces
   t <- P.many parseElemChar
   return (CO (if t == [] then Nothing else Just (T.pack t)))
 
 parseDay :: Parsec Text () DBElem
 parseDay = do
-  P.string "\\dt" >> P.spaces
+  P.string "\\dt" >> spaces
   t <- P.many parseElemChar
   return (DT (if t == [] then Nothing else Just (T.pack t)))
 
 parseHeader :: Parsec Text () DBElem
 parseHeader = do
-  P.string "\\_sh" >> P.spaces
+  P.string "\\_sh" >> spaces
   t <- P.many parseElemChar
   return (HD (T.pack t))
 
